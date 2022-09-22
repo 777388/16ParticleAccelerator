00f57778bf4c15511982893c7bade0f647c177fc
777388 <96343159+777388@users.noreply.github.com>
2022-09-22 10:03:23 -0700

    Update antimatter.py

--- antimatter.py
+++ antimatter.py
@@ -1,41 +1,45 @@
-pimport threading
+print("This may cause a blue light to shoot out of your computer, in case of antimatter creation at point of matter, causing a gamma ray burst followed by memory error")
+import threading
 
 def electron(currentposition, lastposition):
         while True:
                 currentposition = lastposition
                 lastposition = currentposition
                 print(currentposition, end="\r", flush=True)
-if __name__ =="__main__":
-        def knot():
-                t1 = threading.Thread(target=electron, args=(2,1))
-                t2 = threading.Thread(target=electron, args=(1,2))
-                t3 = threading.Thread(target=electron, args=(3,4))
-                t4 = threading.Thread(target=electron, args=(4,3))
-                t1.start()
-                t2.start()
-                t3.start()
-                t4.start()
-                while True:
-                        t1 = t4
-                        print("  t4", end="\r", flush=True)
-                        t2 = t3
-                        print("  t3", end="\r", flush=True)
-                        t4 = t1
-                        print("  t1", end="\r", flush=True)
-                        t3 = t2
-                        print("  t2", end="\r", flush=True)
-        t5 = threading.Thread(target=knot)
-        t6 = threading.Thread(target=knot)
-        t7 = threading.Thread(target=knot)
-        t8 = threading.Thread(target=knot)
-        t5.start()
-        t6.start()
-        t7.start()
-        t8.start()
+def knot(dimension):
+
+        t1 = threading.Thread(target=electron, args=(2,1))
+        t2 = threading.Thread(target=electron, args=(1,2))
+        t3 = threading.Thread(target=electron, args=(3,4))
+        t4 = threading.Thread(target=electron, args=(4,3))
+        t1.start()
+        t2.start()
+        t3.start()
+        t4.start()
+        while True:
+                t1 = t4
+                print("  t4", end="\r", flush=True)
+                t2 = t3
+                print("  t3", end="\r", flush=True)
+                t3 = t2
+                print("  t2", end="\r", flush=True)
+                t4 = t1
+                print("  t1", end="\r", flush=True)
+                print("       "+dimension, end="\r", flush=True)
         t1.join()
         t2.join()
         t3.join()
         t4.join()
+
+if __name__ =="__main__":
+        t5 = threading.Thread(target=knot, args=("dimension 1",))
+        t6 = threading.Thread(target=knot, args=("dimension 2",))
+        t7 = threading.Thread(target=knot, args=("dimension 3",))
+        t8 = threading.Thread(target=knot, args=("dimension 4",))
+        t5.start()
+        t6.start()
+        t7.start()
+        t8.start()
         t5.join()
         t6.join()
         t7.join()
