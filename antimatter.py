696b6c142773476b6bd165f16705fc706edb968e
777388 <96343159+777388@users.noreply.github.com>
2022-09-22 10:32:44 -0700

    Update antimatter.py

--- antimatter.py
+++ antimatter.py
@@ -1,11 +1,23 @@
 print("This may cause a blue light to shoot out of your computer, in case of antimatter creation at point of matter, causing a gamma ray burst followed by memory error")
 import threading
+import signal
+import sys
+store = ""
+store2 = ""
+store3 = ""
+
+def sigint_handler(signal, frame):
+    print("")
+    sys.exit(0)
+    sys.exit(0)
+signal.signal(signal.SIGINT, sigint_handler)
 
 def electron(currentposition, lastposition):
         while True:
                 currentposition = lastposition
                 lastposition = currentposition
                 print(currentposition, end="\r", flush=True)
+                store = lastposition
 def knot(dimension):
 
         t1 = threading.Thread(target=electron, args=(2,1))
@@ -19,13 +31,18 @@ def knot(dimension):
         while True:
                 t1 = t4
                 print("  t4", end="\r", flush=True)
+                store2 = "t4"
                 t2 = t3
                 print("  t3", end="\r", flush=True)
+                store2 = "t3"
                 t3 = t2
                 print("  t2", end="\r", flush=True)
+                store2 = "t2"
                 t4 = t1
                 print("  t1", end="\r", flush=True)
+                store2 = "t1"
                 print("       "+dimension, end="\r", flush=True)
+                store3 = dimension
         t1.join()
         t2.join()
         t3.join()
